f11620248dcc5529f5182f74c68eceae2156f3af
Talat Faheem <110402662+CYBERCODERoss@users.noreply.github.com>
2024-08-24 19:28:10 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// api/index.js
+
 require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
@@ -5,11 +7,10 @@ const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
-const connectDB = require('./server/config/db');
-const { isActiveRoute } = require('./server/helpers/routeHelpers');
+const connectDB = require('../server/config/db');
+const { isActiveRoute } = require('../server/helpers/routeHelpers');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Connect to DB
 connectDB()
@@ -44,9 +45,8 @@ app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 app.locals.isActiveRoute = isActiveRoute; 
 
-app.use('/', require('./server/routes/main'));
-app.use('/', require('./server/routes/admin'));
+app.use('/', require('../server/routes/main'));
+app.use('/', require('../server/routes/admin'));
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+// Export the Express app
+module.exports = app;
